726866532e488f7125605cb5116f76f5edd64c3c
Srinath Rayabarapu <naath.r@gmail.com>
2024-01-08 06:32:43 +0530

    loggers added

--- proj_pdf/src/main/java/com/sri/pdf/DecryptPdf.java
+++ proj_pdf/src/main/java/com/sri/pdf/DecryptPdf.java
@@ -1,17 +1,19 @@
 package com.sri.pdf;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 import java.io.File;
 import java.io.IOException;
 
+@Slf4j
 public class DecryptPdf {
 
     public static void main(String[] args) throws IOException {
-        PDDocument pd = PDDocument.load(new File("/Users/srinath.rayabarapu/Desktop/Home Loans/SBI-Naveen/My-Docs/7. ITR/AY_2022-23_ITR_csbps6004b28121985.pdf"),
-                "csbps6004b28121985");
+        String basePath = "/Users/srinath.rayabarapu/Desktop/Home Loans/SBI-Naveen/My-Docs/7. ITR";
+        PDDocument pd = PDDocument.load(new File(basePath + "/AY_2022-23_ITR_csbps6004b28121985.pdf"), "csbps6004b28121985");
         pd.setAllSecurityToBeRemoved(true);
-        pd.save("/Users/srinath.rayabarapu/Desktop/Home Loans/SBI-Naveen/My-Docs/7. ITR/AY_2022-23_ITR_csbps6004b28121985-Decrypted.pdf");
+        pd.save(basePath + "/AY_2022-23_ITR_csbps6004b28121985-Decrypted.pdf");
+        log.info("Decrypted pdf file successfully!");
     }
-
 }
--- proj_pdf/src/main/java/com/sri/pdf/MergePdf.java
+++ proj_pdf/src/main/java/com/sri/pdf/MergePdf.java
@@ -1,23 +1,28 @@
 package com.sri.pdf;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.pdfbox.io.MemoryUsageSetting;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
 
 import java.io.File;
 import java.io.IOException;
 
+@Slf4j
 public class MergePdf {
     public static void main(String[] args) throws IOException {
+
+        String basePath = "/Users/srinath.rayabarapu/My Drive/Travel Docs/Office/Travel Tickets/17:22-12-23/Bills/21.BLR_Airport-Home";
+
         // loading all the pdf files we wish to merge
-        File file1 = new File("/Users/srinath.rayabarapu/My Drive/Travel Docs/Office/Travel Tickets/17:22-12-23/Bills/21.BLR_Airport-Home/BLR_Airport-Home.pdf");
-        File file2 = new File("/Users/srinath.rayabarapu/My Drive/Travel Docs/Office/Travel Tickets/17:22-12-23/Bills/21.BLR_Airport-Home/BLR_Airport-Home1.pdf");
-        File file3 = new File("/Users/srinath.rayabarapu/My Drive/Travel Docs/Office/Travel Tickets/17:22-12-23/Bills/21.BLR_Airport-Home/BLR_Airport-Home2.pdf");
+        File file1 = new File(basePath + "/BLR_Airport-Home.pdf");
+        File file2 = new File(basePath + "/BLR_Airport-Home1.pdf");
+        File file3 = new File(basePath + "/BLR_Airport-Home2.pdf");
 
         // Instantiating PDFMergerUtility class
         PDFMergerUtility obj = new PDFMergerUtility();
 
         // Setting the destination file path
-        obj.setDestinationFileName("/Users/srinath.rayabarapu/My Drive/Travel Docs/Office/Travel Tickets/17:22-12-23/Bills/21.BLR_Airport-Home/BLR_Airport-Home-All.pdf");
+        obj.setDestinationFileName(basePath + "/BLR_Airport-Home-All.pdf");
 
         // Add all source files, to be merged
         obj.addSource(file1);
@@ -27,7 +32,7 @@ public class MergePdf {
         // Merging documents
         obj.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly());
 
-        System.out.println("PDF Documents merged to a single file");
+        log.info("PDF Documents merged to a Single file!");
     }
 
 }
