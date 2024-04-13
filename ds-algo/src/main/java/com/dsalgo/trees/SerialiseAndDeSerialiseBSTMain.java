5dba26d8cce809c61fcc5354a13121de1be991ae
Srinath Rayabarapu <naath.r@gmail.com>
2024-04-13 11:15:34 +0530

    Serilaizing and Deserilising Binary tree

--- ds-algo/src/main/java/com/dsalgo/trees/SerialiseAndDeSerialiseBSTMain.java
+++ ds-algo/src/main/java/com/dsalgo/trees/SerialiseAndDeSerialiseBSTMain.java
@@ -1,5 +1,8 @@
 package com.dsalgo.trees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * given a binary tree, serialize and deserialize.
  */
@@ -10,23 +13,64 @@ public class SerialiseAndDeSerialiseBSTMain {
         TreeNode root = TreeUtil.create6NodesTree();
         root.leftChild.leftChild.leftChild = new TreeNode(5, "five");
 
-        String serialisedString = serialise(root);
+        List<String> list = new ArrayList<>();
 
-        System.out.println("serialisedString = " + serialisedString);
+        serialise(root, list);
 
-        root = deserialise(serialisedString);
+        System.out.println("list.toArray().toString() = " + list);
 
-        serialisedString = serialise(root);
+        List<Integer> indexList = new ArrayList<>();
+        indexList.add(-1);
+        root = deserialise(list, indexList);
 
-        System.out.println("serialisedString = " + serialisedString);
+        list.clear();
+
+        System.out.println("list.toArray().toString() = " + list);
+
+        serialise(root, list);
+
+        System.out.println("list.toArray().toString() = " + list);
     }
 
-    private static TreeNode deserialise(String serialisedString) {
-        return null;
+    /**
+     * Deserializing from list. Move each element forward once the node is created
+     *
+     * @param list
+     * @param indexList
+     * @return
+     */
+    private static TreeNode deserialise(List<String> list, List<Integer> indexList) {
+
+        indexList.add(0, indexList.get(0)+1);
+
+        if("N".equals(list.get(indexList.get(0)))){
+            return null;
+        }
+
+        TreeNode node = new TreeNode(Integer.valueOf(list.get(indexList.get(0))));
+
+        node.leftChild = deserialise(list, indexList);
+        node.rightChild = deserialise(list, indexList);
+
+        return node;
     }
 
-    private static String serialise(TreeNode root) {
-        return null;
+    /**
+     * serializing in pre-order traversal
+     *
+     * @param root
+     * @param list
+     */
+    private static void serialise(TreeNode root, List<String> list) {
+
+        if (root == null){
+            list.add("N");
+            return;
+        }
+
+        list.add(root.data+"");
+        serialise(root.leftChild, list);
+        serialise(root.rightChild, list);
     }
 
 }
